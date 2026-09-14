785c4b6f575ff88c2e0486417305ebb912486837
HakiBl4ck <53626680+HakiBl4ck@users.noreply.github.com>
2026-09-13 21:03:54 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // 3. Reemplaza la clave abajo
 // 4. Crea un "Bin" y copia el ID, o deja vacío para crear uno nuevo
 
-const JSONBIN_API_KEY = "";  // Tu API key de jsonbin.io
+const JSONBIN_API_KEY = "$2a$10$qQxqe1.V1cF05fl6Jwk71eM3lv7i36nrYNWldY9bWdscThiKfZa9y";  // Tu API key de jsonbin.io
 const JSONBIN_API_URL = "https://api.jsonbin.io/v3";
 
 // Si no hay API key, usa localStorage como fallback
